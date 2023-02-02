2b226ead77a3fc31422169535c7878a426a8e0bd
Ihaza Omosefe <iomosefe2016@gmail.com>
2023-02-02 14:14:24 +0100

    Fix image issue

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
           <div className="">
           <img
             className="rounded-lg"
-            src="..\src\assets\images\image-qr-code.png"
+            src="..\public\src\assets\images\image-qr-code.png"
             alt="QR CODE"
           />
           </div>
